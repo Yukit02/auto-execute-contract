41938adfbd8e48ab5c41863dc92414a361dbef68
Yukit02 <yukijobit02@gmail.com>
2023-01-23 16:25:07 +0400

    feat: finalize block logic

--- src/contract.rs
+++ src/contract.rs
@@ -1,5 +1,5 @@
 #[cfg(not(feature = "library"))]
-use cosmwasm_std::{entry_point, DepsMut, Env, MessageInfo, Response, StdError};
+use cosmwasm_std::{entry_point, DepsMut, Env, MessageInfo, Response, StdError, WasmMsg, to_binary};
 use cw2::set_contract_version;
 
 use sei_cosmwasm::{SeiQueryWrapper};
@@ -11,6 +11,9 @@ use crate::sei_type::*;
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:auto-execute-contract";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+// constants
+const ODHIO_CONTRACT_ADDR: &str = "sei14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sh9m79m";
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -43,9 +46,17 @@ pub mod sudo_execution {
   pub fn finalize_block(
     _deps: DepsMut<SeiQueryWrapper>,
     env: Env,
-    _contract_order_results: Vec<ContractOrderResult>
+    contract_order_results: Vec<ContractOrderResult>
   ) -> Result<Response, StdError> {
-    if env.block.height % 10 == 0 {}
+    if env.block.height % 10 == 0 {
+
+      let msg = format!("{{ 'IncrementAdv': {{ 'contract_order_results' : {:?} }} }}", contract_order_results);
+      WasmMsg::Execute {
+          contract_addr: ODHIO_CONTRACT_ADDR.into(),
+          msg: to_binary(&msg)?,
+          funds: vec![],
+      };
+    }
 
     Ok(Response::new())
   }
